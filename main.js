0b34d1c4974bac4f24617c579a9b4ec12d7a5c07
taiyo8911 <68884687+taiyo8911@users.noreply.github.com>
2023-04-17 12:35:29 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-//現在の損益率を計算する
-function calc1() {
-  var now_price = document.getElementById("nowprice").value;
-  var tanka = document.getElementById("tanka").value;
-  var result = (now_price / tanka - 1) * 100;
-  document.getElementById("result_area").textContent = result + "％";
+// 現在の損益率を計算する
+function calculateProfitRate() {
+  const currentPrice = document.getElementById("currentPrice").value;
+  const purchasePrice = document.getElementById("purchasePrice").value;
+  const profitRate = ((currentPrice / purchasePrice) - 1) * 100;
+  document.getElementById("result_area").textContent = `${profitRate.toFixed(2)}％`;
 }
 
-//利益率から売値を計算する
-function calc2() {
-  var tanka2 = document.getElementById("tanka2").value;
-  var profitRate = document.getElementById("profitRate").value;
-  var rate = Number(tanka2) * Number(profitRate) / 100;
-  var result2 = rate + Number(tanka2);
-  document.getElementById("result_area2").innerHTML = result2 + "円で売りましょう。";
+
+// 利益率から売値を計算する
+function calculateSellPriceFromProfitRate() {
+  const purchasePrice = Number(document.getElementById("purchasePrice2").value);
+  const profitRate = Number(document.getElementById("profitRate2").value);
+  const sellPrice = (purchasePrice * (100 + profitRate)) / 100;
+  document.getElementById("result_area2").innerHTML = `${sellPrice.toFixed(0)}円で売りましょう。`;
 }
 
-//目標金額から売値を計算する
-function calc3() {
-  var tanka3 = document.getElementById("tanka3").value;
-  var sellAmount = document.getElementById("sellAmount").value;
-  var targetAmount = document.getElementById("targetAmount").value;
 
-  var ans1 = Number(targetAmount) + (Number(tanka3) * Number(sellAmount));
-  var ans2 = Number(ans1) / Number(sellAmount);
-  document.getElementById("result_area3").innerHTML = ans2 + "円で売りましょう。";
+// 目標金額から売値を計算する
+function calculateSellPriceFromTargetAmount() {
+  const purchasePrice = Number(document.getElementById("purchasePrice3").value);
+  const sellingAmount = Number(document.getElementById("sellingAmount").value);
+  const targetAmount = Number(document.getElementById("targetAmount").value);
+
+  const sellPrice = (targetAmount + (purchasePrice * sellingAmount)) / sellingAmount;
+  document.getElementById("result_area3").innerHTML = `${sellPrice.toFixed(0)}円で売りましょう。`;
 }
